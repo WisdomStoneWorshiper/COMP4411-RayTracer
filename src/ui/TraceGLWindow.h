ef1e3a2b1d9cbe78ed703263e819dda0f1f1265e
Johnny Lo <klloaj@connect.ust.hk>
2021-03-22 10:21:55 +0800

    initial commit

---
+++ src/ui/TraceGLWindow.h
@@ -0,0 +1,44 @@
+//
+// originalview.h
+//
+// The header file of the view for rendered image
+//
+
+#ifndef __TRACEGLWINDOW_H__
+#define __TRACEGLWINDOW_H__
+
+#include <FL/Fl.H>
+#include <FL/Fl_Gl_Window.H>
+#include <FL/gl.h>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#include <GL/gl.h>
+#include <GL/glu.h>
+#else
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#endif
+
+#include "../RayTracer.h"
+
+class TraceGLWindow : public Fl_Gl_Window {
+   public:
+	TraceGLWindow(int x, int y, int w, int h, const char *l);
+	void draw();
+	int handle(int event);
+
+	RayTracer *raytracer;
+
+	void refresh();
+
+	void resizeWindow(int width, int height);
+
+	void saveImage(char *iname);
+
+	void setRayTracer(RayTracer *tracer);
+
+   private:
+	int m_nWindowWidth, m_nWindowHeight;
+	int m_nDrawWidth, m_nDrawHeight;
+};
+
+#endif	// __TRACE_GL_WINDOW_H__
